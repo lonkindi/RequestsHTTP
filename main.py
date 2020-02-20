5658cebb8f6f173b0cf21a750f973ccfadd47890
lonkindi <legions.ru@gmail.com>
2020-02-20 11:10:36 +0500

    Reformat files Task No. 1 completed

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import requests
-#  документация https://yandex.ru/dev/translate/doc/dg/reference/translate-docpage/
+import os
 
 API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
-def translate_it(text, to_lang):
-    """
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-    :param to_lang:
-    :return:
-    """
 
+def translate_it(path_source, path_result, from_lang, to_lang='ru'):
+    with open(path_source, encoding='utf-8') as file_s:
+        text_s = file_s.read()
     params = {
         'key': API_KEY,
-        'text': text,
-        'lang': 'ru-{}'.format(to_lang),
+        'text': text_s,
+        'lang': '{}-{}'.format(from_lang, to_lang),
     }
-
     response = requests.get(URL, params=params)
     json_ = response.json()
-    return ''.join(json_['text'])
+    #print(params)
+    text_r = ''.join(json_['text'])
 
+    with open(path_result, 'w', encoding='utf-8') as file_r:
+        file_r.write(text_r)
+        print(f'Файл : {path_source} переведён.\nРезультат перевода записан в : {path_result}')
 
-# print(translate_it('В настоящее время доступна единственная опция — признак включения в ответ автоматически
-# определенного языка переводимого текста. Этому соответствует значение 1 этого параметра.', 'no'))
 
 if __name__ == '__main__':
-    print(translate_it('привет', 'en'))
+    trnsl_to = 'ru'
+    file_list = os.listdir('source_files')
+    for file in file_list:
+        s_file_name = file
+        s_full_path = os.path.join('source_files', s_file_name)
+        trnsl_from = file[0:2].lower()
+        r_file_name = f'translate_{trnsl_from}_{trnsl_to}.txt'
+        r_full_path = os.path.join('result_files', r_file_name)
+        translate_it(s_full_path, r_full_path, trnsl_from, trnsl_to)
